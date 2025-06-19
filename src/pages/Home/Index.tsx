669ae236593f7343b23f8e78120c2a2a1309baa4
saminyasar004 <yasarsamin57@gmail.com>
2025-06-19 22:42:24 +0600

    dev: home page added

---
+++ src/pages/Home/Index.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/pages/Home/Hero";
+import BrandPartners from "./BrandPartners";
+import Feature from "./Feature";
+import Process from "./Process";
+
+export default function Home() {
+	return (
+		<>
+			<Hero />
+			<BrandPartners />
+			<Feature />
+			<Process />
+		</>
+	);
+}
