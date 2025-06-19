a47b1679c147c016536b40e03573b1e63035e3ee
saminyasar004 <yasarsamin57@gmail.com>
2025-06-19 22:41:44 +0600

    dev: section heading component implemented

---
+++ src/components/common/SectionHeading.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from "react";
+
+export default function SectionHeading({
+	title,
+	description,
+}: {
+	title: ReactNode;
+	description: string;
+}) {
+	return (
+		<div className="w-full flex flex-col gap-3 items-center justify-center">
+			{title && (
+				<h2 className="font-bold text-5xl text-center leading-tight">
+					{typeof title === "string" ? title : <>{title}</>}
+				</h2>
+			)}
+			{description && (
+				<p className="text-center w-[55%] leading-normal">
+					{description}
+				</p>
+			)}
+		</div>
+	);
+}
