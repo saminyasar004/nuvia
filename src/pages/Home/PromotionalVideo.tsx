ca6a8f404ae3ea5a297b1370ff28fa1e5f5b6993
saminyasar004 <yasarsamin57@gmail.com>
2025-06-25 17:26:25 +0600

    dev: promotional video src moved into local assets folder.

--- src/pages/Home/PromotionalVideo.tsx
+++ src/pages/Home/PromotionalVideo.tsx
@@ -1,13 +1,11 @@
 import VideoPlayer from "@/components/common/VideoPlayer";
+import VideoSrc from "@/assets/videos/promotional-video.mp4";
 
 export default function PromotionalVideo() {
 	return (
 		<section className="py-24">
 			<div className="container">
-				<VideoPlayer
-					src="https://res.cloudinary.com/dhwkkvjmb/video/upload/v1750759377/promotional-video_gswlvf.mp4"
-					// poster="https://www.w3schools.com/html/mov_bbb.jpg"
-				/>
+				<VideoPlayer src={VideoSrc} />
 			</div>
 		</section>
 	);
